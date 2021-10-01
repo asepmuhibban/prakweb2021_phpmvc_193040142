595ff5b61abcd881e79d5ce5029ca1fe3b1620ea
asepmuhibban <asepmuhibban82@gmail.com>
2021-10-01 20:58:58 +0700

    Menyelesaikan video View

---
+++ app/views/about/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Halaman About</title>
+</head>
+<body>
+    <h1>About Me</h1>
+    <p>Halo, nama saya <?= $data['nama']; ?>, <?= $data['umur']; ?> saya 32 tahun, saya adalah seorang <?= $data['pekerjaan']; ?>.</p>
+</body>
+</html>
